d367abd04e31cd63077440e4c85dba5af887bc11
Thomas Hamburger <hamburgerthomas@gmail.com>
2016-02-01 14:15:30 +0100

    Possible to fill in name

--- demo 1/ViewController.swift
+++ demo 1/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var name: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        topLabel.text = "Enter your name"
     }
 
     override func didReceiveMemoryWarning() {
